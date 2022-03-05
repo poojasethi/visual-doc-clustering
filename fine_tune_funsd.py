091483ae12b60b18780d588011045c56e92bfc57
Bryan Chia <bryancws@stanford.edu>
2022-03-04 19:33:16 -0600

    Create fine_tune_funsd.py

---
+++ fine_tune_funsd.py
@@ -0,0 +1,20 @@
+from LayoutLM import LayoutLM
+import glob
+import json
+
+if __name__ == '__main__':
+
+    directory = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/funsd/training_data/annotations'
+    
+    #Process dataset
+    i1 = LayoutLM()
+    processed_data = i1.process_json(directory, 'text', 'box', 'label', funsd = True)
+
+    #Encode data
+    outpath = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/funsd_enc'
+    labels_dict = {'question':0, 'answer':1, 'header':2, 'other':3}
+    encoded_data = i1.get_encodings(outpath, labels = labels_dict)
+
+    #Fine-tune dataset
+    model_save_path = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/models/fine_tuned_unrelated'
+    i1.fine_tune(outpath, model_save_path, token = True, k = 5, batch_size = 2, num_train_epochs=50)
