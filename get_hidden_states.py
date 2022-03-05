0ae28637038bc22cf66ce62763ff457fe157f347
Bryan Chia <bryancws@stanford.edu>
2022-03-05 01:32:33 -0600

    Create get_hidden_states.py

---
+++ get_hidden_states.py
@@ -0,0 +1,34 @@
+from LayoutLM import LayoutLM
+import glob
+import json
+
+if __name__ == '__main__':
+
+    #PUT DIRECTORY OF RIVLETS HERE
+    directory = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/rivlets'
+
+    #OUTPUT VANILLA LM HIDDEN STATES
+    i1 = LayoutLM()
+    int_data = i1.process_json(directory, 'processed_word', 'location', position_processing = True)
+    outpath = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/encodings/layoutlm_noft_encodings.pkl'
+    encodings = i1.get_encodings()
+    hidden_state = i1.get_hidden_state(outpath = outpath)
+    print(hidden_state.to_pandas())
+
+    #OUTPUT FINE-TUNED HIDDEN STATES (RELATED TASK)
+    i2 = LayoutLM()
+    i2.process_json(directory, 'processed_word', 'location', position_processing = True)
+    outpath = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/encodings/layoutlm_ft_encodings.pkl'
+    model_path = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/models/fine_tuned_related/epoch7'
+    encodings = i2.get_encodings()
+    hidden_state = i2.get_hidden_state(outpath = outpath, model_path = model_path)
+    print(hidden_state.to_pandas())
+
+    #OUTPUT FINE-TUNED HIDDEN STATES (UNRELATED TASK)
+    i3 = LayoutLM()
+    i3.process_json(directory, 'processed_word', 'location', position_processing = True)
+    outpath = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/encodings/layoutlm_ft_ur_encodings.pkl'
+    model_path = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/models/fine_tuned_unrelated/epoch15'
+    encodings = i3.get_encodings()
+    hidden_state = i3.get_hidden_state(outpath = outpath, model_path = model_path)
+    print(hidden_state.to_pandas())
