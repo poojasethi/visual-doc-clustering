9124fa3ed1d08089f3a40f5ad8143762f6026cb1
Pooja Sethi <sethi.pooja@gmail.com>
2022-03-11 21:31:51 -0800

    Apply formatting to LayoutLMv2 with black

--- lib/LayoutLMv2.py
+++ lib/LayoutLMv2.py
@@ -1,23 +1,38 @@
-from transformers import LayoutLMv2Processor, LayoutLMv2Model
-from PIL import Image
-import torch
-from typing import List, Tuple, Dict, Set, Union
-import json
 import glob
+import json
 import re
 from pathlib import Path
-import pandas as pd
+from typing import Dict, List, Set, Tuple, Union
+
 import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+from transformers import LayoutLMv2Model, LayoutLMv2Processor
+
 from datasets import Array2D, Array3D, Array4D, ClassLabel, Dataset, Features, Sequence, Value, concatenate_datasets
 
-class LayoutLMv2():
-    """ Get embeddings for LayoutLMv2
-    """
-    def __init__(self, vocab_size = 30522, hidden_size = 768, num_hidden_layers = 12,\
-        num_attention_heads = 12, intermediate_size = 3072, hidden_act = 'gelu', hidden_dropout_prob = 0.1,\
-            attention_probs_dropout_prob = 0.1, max_position_embeddings = 512, type_vocab_size = 2,\
-                initializer_range = 0.02, layer_norm_eps = 1e-12, pad_token_id = 0, \
-                    max_2d_position_embeddings = 1024):
+
+class LayoutLMv2:
+    """Get embeddings for LayoutLMv2"""
+
+    def __init__(
+        self,
+        vocab_size=30522,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.1,
+        max_position_embeddings=512,
+        type_vocab_size=2,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
+        pad_token_id=0,
+        max_2d_position_embeddings=1024,
+    ):
 
         # Initializing a LayoutLM configuration with default values
         self.vocab_size = vocab_size
@@ -42,7 +57,7 @@ class LayoutLMv2():
         self.encoding = pd.DataFrame()
 
     def __get_encodings(self, example):
-        
+
         image = Image.open(example["image_path"]).convert("RGB")
         encoding = self.processor(image, padding="max_length", truncation=True)
         example["token_type_ids"] = encoding.token_type_ids[0]
@@ -52,14 +67,13 @@ class LayoutLMv2():
         example["input_ids"] = encoding.input_ids[0]
         return example
 
-    def __get_hidden_states(self, example, model = None):
+    def __get_hidden_states(self, example, model=None):
         image = Image.open(example["image_path"]).convert("RGB")
-        outputs = self.model(**self.processor(image, padding="max_length", truncation=True, return_tensors = "pt"))
+        outputs = self.model(**self.processor(image, padding="max_length", truncation=True, return_tensors="pt"))
         example["last_hidden_state"] = outputs.last_hidden_state[0]
         return example
 
-
-    def get_outputs(self, directory, model = None, outpath = None, file_type = "png"):
+    def get_outputs(self, directory, model=None, outpath=None, file_type="png"):
 
         # Import Files
         path = Path(directory)
@@ -69,31 +83,34 @@ class LayoutLMv2():
         else:
             files = [path]
 
-        #Select just one image to use -- need to think about whether we want to use more than 1 page
-        #Should we concatenate the two hidden states?
-        image_paths = [re.match(r"(.+)/pages/0/(.*)", str(f)).group(0) for f in [*files] \
-            if re.match(r"(.+)/pages/0/(.*)", str(f)) is not None]
+        # Select just one image to use -- need to think about whether we want to use more than 1 page
+        # Should we concatenate the two hidden states?
+        image_paths = [
+            re.match(r"(.+)/pages/0/(.*)", str(f)).group(0)
+            for f in [*files]
+            if re.match(r"(.+)/pages/0/(.*)", str(f)) is not None
+        ]
 
-        #Testing a smaller number of images at the moment
+        # Testing a smaller number of images at the moment
         i = 0
         for ip in image_paths[:5]:
             self.encoding.at[i, "image_path"] = ip
             i += 1
 
-        #Encode via mapping
+        # Encode via mapping
         self.encoding = Dataset.from_pandas(self.encoding).remove_columns("__index_level_0__")
-            
+
         features = Features(
             {
-            #"image": Sequence(Sequence(Sequence(Sequence(Value(dtype="uint8"))))),
-            "image": Array3D(dtype="int64", shape=(3, 224, 224)),
-            'input_ids': Sequence(Value(dtype='int64')),
-            "bbox": Array2D(dtype="int64", shape=(512, 4)),
-            "attention_mask": Sequence(Value(dtype="int64")),
-            "token_type_ids": Sequence(Value(dtype="int64")),
-            "image_path": Value(dtype="string"),
+                # "image": Sequence(Sequence(Sequence(Sequence(Value(dtype="uint8"))))),
+                "image": Array3D(dtype="int64", shape=(3, 224, 224)),
+                "input_ids": Sequence(Value(dtype="int64")),
+                "bbox": Array2D(dtype="int64", shape=(512, 4)),
+                "attention_mask": Sequence(Value(dtype="int64")),
+                "token_type_ids": Sequence(Value(dtype="int64")),
+                "image_path": Value(dtype="string"),
             }
-            )    
+        )
 
         self.int_data = self.encoding.map(lambda example: self.__get_encodings(example), features=features)
 
@@ -101,16 +118,19 @@ class LayoutLMv2():
             model = self.model
         self.fin_data = self.int_data.map(lambda example: self.__get_hidden_states(example, model))
 
-        self.fin_data.set_format(type="torch", columns=["image", "bbox", "attention_mask", "token_type_ids", "input_ids"])
+        self.fin_data.set_format(
+            type="torch", columns=["image", "bbox", "attention_mask", "token_type_ids", "input_ids"]
+        )
 
         if outpath is not None:
             self.fin_data.to_pandas().to_pickle(outpath)
 
         return self.fin_data
 
-if __name__ == '__main__':
 
-    directory = '/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/files'
+if __name__ == "__main__":
+
+    directory = "/Users/bryanchia/Desktop/stanford/classes/cs/cs224n/project/data/files"
 
     i2 = LayoutLMv2()
     encodings = i2.get_outputs(directory)
